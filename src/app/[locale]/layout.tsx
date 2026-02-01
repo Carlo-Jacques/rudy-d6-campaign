ddf5825202b287b19738ddbb74e95befedbccb99
Carlo-Jacques <carlo@charlottesweb.design>
2026-01-31 21:57:40 -0500

    Param Promise in layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -96,7 +96,7 @@ export default async function LocaleLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }) {
   const { locale } = await params;
 
