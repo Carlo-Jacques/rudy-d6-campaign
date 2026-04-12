277e38559660d1227a54049b40c95ff7c688d06b
Carlo-Jacques <carlo@charlottesweb.design>
2026-04-12 14:19:24 -0400

    PEtition update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -73,8 +73,8 @@ export default function Hero() {
                         <div className="rounded-3xl border border-black/10 bg-white p-6 sm:p-8 md:-mt-36" style={{ display: 'flex', flexWrap: 'wrap', zIndex: 1 }}>
                             <div className="w-full flex flex-col justify-center items-center">
                                 <img
-                                    src="/img/gallery/Petition%20Update.png"
-                                    alt="Petition Qualified Update"
+                                    src="/img/gallery/Mission%20accomplished_%20petition%20success%20announced.png"
+                                    alt="Mission accomplished, petition success announced"
                                     className="w-full h-auto rounded-lg object-contain border border-black/10 shadow-sm"
                                 />
                             </div>
