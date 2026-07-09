9767cec809d61407bdb23f4f2afda65c2f55ec42
Carlo-Jacques <carlo@charlottesweb.design>
2026-07-09 15:10:21 -0400

    Removed number

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -49,7 +49,7 @@ export default function Footer() {
               <p>14611 Southern Boulevard</p>
               <p>Unit 634</p>
               <p>Loxahatchee, FL 33470</p>
-              <p>Phone: <a href="tel:5616601580" className="font-semibold text-white/80 hover:text-white transition-colors">(561) 660-1580</a> | <a href="tel:5616602436" className="font-semibold text-white/80 hover:text-white transition-colors">(561) 660-2436</a></p>
+              <p>Phone: <a href="tel:5616602436" className="font-semibold text-white/80 hover:text-white transition-colors">(561) 660-2436</a></p>
               <p>E-mail: <a href="mailto:info@rudolphtinker.com" className="text-sm font-medium text-white/80 hover:text-white transition-colors">info@rudolphtinker.com</a></p>
             </div>
             <div className="mt-6 flex flex-col gap-2">
